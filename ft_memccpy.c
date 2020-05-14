b738c229d736e161f891e23cda7a37ef10ec5913
Animoke <animoke@protonmail.com>
2020-05-14 23:31:55 +0200

    fixed cast error in memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: gpatingr <gpatingr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 22:56:46 by gpatingr          #+#    #+#             */
-/*   Updated: 2020/05/14 23:19:38 by gpatingr         ###   ########.fr       */
+/*   Updated: 2020/05/14 23:30:56 by gpatingr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	while (i < n)
 	{
 		d[i] = s[i];
-		if (s[i] == (unsigned char *)c)
+		if (s[i] == (unsigned char)c)
 			return ((void *)&d[i + 1]);
 		i++;
 	}
