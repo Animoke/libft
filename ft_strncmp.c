f08636297b3eae1ca8cc275f94ad40ac69b2e481
Animoke <animoke@protonmail.com>
2020-05-21 00:27:41 +0200

    ft_isprint & ft_strncmp correction

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: gpatingr <gpatingr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 02:18:30 by gpatingr          #+#    #+#             */
-/*   Updated: 2020/04/30 17:40:42 by gpatingr         ###   ########.fr       */
+/*   Updated: 2020/05/21 00:23:18 by gpatingr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,22 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	int				res;
+	unsigned char	*a1;
+	unsigned char	*a2;
 
-	i = 0;
-	if (n == 0)
-		return (0);
-	while (s1[i] == s2[i] && s1[i] && s2[i] && i < n - 1)
-		i++;
-	return (s1[i] - s2[i]);
+	res = 0;
+	a1 = (unsigned char *)s1;
+	a2 = (unsigned char *)s2;
+	while ((n != 0) && (*a1 != 0) && (*a1 == *a2))
+	{
+		a1++;
+		a2++;
+		n--;
+	}
+	if (n != 0)
+	{
+		res = (*a1 - *a2);
+	}
+	return (res);
 }
