afd07b72ce5557a285e70b6345326d6d4178ebe3
Animoke <animoke@protonmail.com>
2020-05-21 00:06:57 +0200

    ft_split test

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: gpatingr <gpatingr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/06 18:51:27 by gpatingr          #+#    #+#             */
-/*   Updated: 2020/05/14 18:45:25 by gpatingr         ###   ########.fr       */
+/*   Updated: 2020/05/21 00:04:01 by gpatingr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
 #include "libft.h"
 
+/*
 static	int		set_checker(const char *set, const char c)
 {
 	size_t	i;
@@ -42,3 +42,64 @@ char	**ft_split(char const *s, char c)
 		return (NULL);
 	return (NULL);
 }*/
+
+
+static size_t	ft_split_counter(char const *s, char c)
+{
+	size_t	splits;
+	int		i;
+
+	splits = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			i++;
+			continue ;
+		}
+		splits++;
+		while (s[i] && s[i] != c)
+			i++;
+	}
+	return (splits);
+}
+
+static void		*ft_free_strings(char **strings)
+{
+	if (strings == NULL)
+		return (NULL);
+	while (*strings != NULL)
+		free(*strings++);
+	free(strings);
+	return (NULL);
+}
+
+char			**ft_split(char const *s, char c)
+{
+	char	**strs;
+	size_t	a;
+	size_t	i;
+	size_t	j;
+
+	if (s == NULL)
+		return (NULL);
+	a = ft_split_counter(s, c);
+	if ((strs = (char**)malloc(sizeof(char*) * (a + 1))) == NULL)
+		return (NULL);
+	a = 0;
+	j = -1;
+	while (s[++j])
+	{
+		if (s[j] == c)
+			continue ;
+		i = 0;
+		while (s[j + i] && s[j + i] != c)
+			i++;
+		if ((strs[a++] = ft_substr(s, j, i)) == NULL)
+			return (ft_free_strings(strs));
+		j += i - 1;
+	}
+	strs[a] = NULL;
+	return (strs);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gpatingr <gpatingr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 12:34:12 by gpatingr          #+#    #+#             */
-/*   Updated: 2020/05/14 18:31:48 by gpatingr         ###   ########.fr       */
+/*   Updated: 2020/05/20 13:53:12 by gpatingr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-//char	**ft_split(char const *s, char c);
+char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
