4d4731d5cbc88e6e17b0951983efe2a30354731f
Gabriel PATINGRE <gpatingr@e3r3p3.42.fr>
2020-08-04 18:18:12 +0200

    substr corrige

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gpatingr <gpatingr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 02:29:23 by gpatingr          #+#    #+#             */
-/*   Updated: 2020/08/04 17:08:07 by gpatingr         ###   ########.fr       */
+/*   Updated: 2020/08/04 18:11:03 by gpatingr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	srclen;
 
 	srclen = ft_strlen(src);
-	if (!dst || !src)
-		return (0);
 	if (srclen + 1 < size)
 		ft_memcpy(dst, src, srclen + 1);
 	else if (size != 0)
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: gpatingr <gpatingr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/03 21:42:02 by gpatingr          #+#    #+#             */
-/*   Updated: 2020/08/04 17:04:07 by gpatingr         ###   ########.fr       */
+/*   Updated: 2020/08/04 18:07:53 by gpatingr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*res;
 
 	i = 0;
-	if (start >= ft_strlen(s) || !s)
+	if (!s)
 		return (NULL);
 	if (!(res = malloc(sizeof(char) * len + 1)))
 		return (NULL);
-	while (s[start] != '\0' && i < len)
+	if (start >= ft_strlen(s))
+	{
+		res[0] = 0;
+		return (res);
+	}
+	while (s[start] && i < len)
 		res[i++] = s[start++];
 	res[i] = '\0';
 	return (res);
